d8ce57fa6c1d19f5b159add17008f856a3f1ab43
Jerek0 <jeremy.minie@gmail.com>
2015-10-30 16:31:43 +0100

    Begin android

---
+++ Assets/TouchButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TouchButton : MonoBehaviour, IPointerDownHandler {
+
+    Image image;
+
+	// Use this for initialization
+	void Start () {
+
+        image = GetComponent<Image>();
+
+	}   
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData) {       
+        Debug.Log("pointer down");
+    }
+}
